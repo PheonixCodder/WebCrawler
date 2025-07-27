9c37b388b69efedf2865db354178864681fba299
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-07-28 00:22:17 +0500

    Completed

--- crawl.js
+++ crawl.js
@@ -1,26 +1,51 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (currentURLObj.hostname !== baseURLObj.hostname) {
+        return pages
+    }
+
+    const normalizeCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizeCurrentURL] > 0 ) {
+        pages[normalizeCurrentURL]++
+        return pages
+    }
+    pages[normalizeCurrentURL] = 1
+
+    console.log(
+        `Crawling ${currentURL}...`
+    )
+
     try {
         const resp = await fetch(currentURL);
 
         if (resp.status > 399){
             console.log(`HTTP error! status: ${resp.status}`);
-            return;
+            return pages
         }
 
         const contentType = resp.headers.get("Content-Type");
 
         if (!contentType.includes('text/html')){
             console.log(`Non-HTML content type: ${contentType}`);
-            return;
+            return pages
         }
 
 
-        console.log( await resp.text())
+        const htmlBody = await resp.text();
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages);
+        }
+
     } catch (error) {
         console.log(`error in fetch : ${error}`);
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseUrl) {
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const {crawlPage} = require("./crawl");
+const { printReport } = require("./report");
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("No website(s) provided");
     process.exit(1);
   }
   const baseUrl = process.argv[2];
 
-  crawlPage(baseUrl);
+  const pages = await crawlPage(baseUrl, baseUrl, {});
+  
+  printReport(pages)
 }
 
 main();
